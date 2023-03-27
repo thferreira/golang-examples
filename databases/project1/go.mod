72e68d18af3b2f5987a55145b4b49a6c7148ff79
Thiago Ferreira <thferreiradev@gmail.com>
2023-03-27 15:12:50 -0300

    Update package project1

--- databases/project1/go.mod
+++ databases/project1/go.mod
@@ -2,4 +2,7 @@ module github.com/thferreira/golang-examples/databases/project1
 
 go 1.20
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/google/uuid v1.3.0
+)
--- databases/project1/main.go
+++ databases/project1/main.go
@@ -1,6 +1,10 @@
-package project1
+package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 type Product struct {
 	ID    string
@@ -17,5 +21,49 @@ func NewProduct(name string, price float64) *Product {
 }
 
 func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/golang-examples")
 
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	product := NewProduct("Notebook", 1899.90)
+
+	err = insertProduct(db, product)
+
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("insert into products(id, name, price) values(?, ?, ?)")
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("update products set name = ?, price = ? where id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
